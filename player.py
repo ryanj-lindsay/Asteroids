d36c2c84965ae406b5d99c0694664d0fe8beeee6
ryanj.lindsay <lindsayryan0@gmail.com>
2025-06-12 10:03:03 -0600

    Cleaned a few areas in player.py

--- player.py
+++ player.py
@@ -21,21 +21,19 @@ class Player(CircleShape):
         
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        return self.rotation
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotation = self.rotate(dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
-            self.rotation = self.rotate(-dt)
+            self.rotate(dt)
         if keys[pygame.K_w]:
-            self.position = self.move(dt)
+            self.move(dt)
         if keys[pygame.K_s]:
-            self.position = self.move(-dt) 
+            self.move(-dt) 
             
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        return self.position
